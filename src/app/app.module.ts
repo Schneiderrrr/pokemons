e32c289b9d5e1be33aae76c7ed797e498ba1b202
Schneiderrrr <kat1906200152@gmail.com>
2024-08-28 17:46:05 +0300

    rebase fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
+import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PokemonListComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule
 ],
