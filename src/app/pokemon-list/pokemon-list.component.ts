c913731d6df9ce1b39cdf0098f168c72c8f610ff
Schneiderrrr <kat1906200152@gmail.com>
2024-08-29 14:11:52 +0300

    Добавила возможность передать значение текущей страницы через url + исправление передачи параметра offset

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,18 +8,23 @@ import { ApiService } from '../api.service';
   styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent implements OnInit {
+  readonly limit: number = 12;
+
   pokemons: any[] = [];
-  page = 1;
+  page: number = 1;
   totalPokemons: number = 0;
   
-  constructor(private apiservice: ApiService){}
+  constructor(private apiservice: ApiService, private route: ActivatedRoute){}
   
   ngOnInit(): void {
    this.getPokemons();
   }
 
   getPokemons() {
-    this.apiservice.get(this.page + 0, 10).then((response: any) => {
+    if (this.route.snapshot.queryParams["page"] != null)
+      this.page = parseInt(this.route.snapshot.queryParams["page"]);
+
+    this.apiservice.get(this.page * this.limit, this.limit).then((response: any) => {
       this.totalPokemons = response.count;
 
       response.results.forEach((responseElem: any) => {
